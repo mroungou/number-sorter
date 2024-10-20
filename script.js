942b2990691cf45ab767d5937d03c2d285d9bcf4
Malick Roungou <mjbroungou@gmail.com>
2024-10-20 09:43:15 -0400

    updates

--- script.js
+++ script.js
@@ -19,14 +19,23 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length - 1; j++) {
-            console.log(array, array[j], array[j+1])
+            // console.log(array, array[j], array[j+1])
             if (array[j] > array[j + 1]) {
+                // temp holds the larger value
                 const temp = array[j]
+                // the current value is then replaced by the smaller value which is j + 1
                 array[j] = array[j + 1];
+                // the larger value is then "bubbled" to the towards the end -> the value of temp holds the larger value
                 array[j + 1] = temp;
             }
         }
     }
+
+    return array;
+}
+
+const selectionSort = (array) => {
+    
 }
 
 sortButton.addEventListener('click', sortInputArray)
